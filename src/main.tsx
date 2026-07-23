56fcb6de129482ef7391e943cfa75c5962164ec8
Lars Moelleken <lars@moelleken.org>
2026-07-23 21:12:20 +0200

    Render the updated CV page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import CvPage from './CvPage.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <CvPage />
   </StrictMode>,
 );
