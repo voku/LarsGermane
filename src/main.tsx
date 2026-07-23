cc9ef4e8db2ca48a646eb66ac38f82c31f82466a
Lars Moelleken <lars@moelleken.org>
2026-07-23 22:08:45 +0200

    fix: restore existing App entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import CvPage from './CvPage.tsx';
+import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <CvPage />
+    <App />
   </StrictMode>,
 );
